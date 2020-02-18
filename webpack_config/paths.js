adcd9e5bf091252a238a743a674b7ffe96f2aff8
Stefan Petkovic <stefan.petkovic@diwanee.com>
2020-02-18 17:06:41 +0100

    gh pages

--- webpack_config/paths.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: path.resolve(__dirname, '../src/main.js'),
-    output: path.resolve(__dirname, '../html'),
-    cssOutput: 'css/styles.css',
-    jsOutput: 'js/master.bundle.js'
-};
